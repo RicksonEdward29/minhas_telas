da209a4ac431cd83c9d57edb3ed847123ef26dd1
RicksonEdward29 <ricksonsilva29@outlook.com>
2022-04-02 02:38:44 -0300

    Primeira tela onde criei até a parte do horário e tbm adicioneiuma biblioteca para colocar icones na tela

--- App.js
+++ App.js
@@ -2,33 +2,59 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, Text, View, StyleSheet} from 'react-native';
 import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/AntDesign'
 
 const Page =styled.SafeAreaView`
-  flex:1;
-  flex-direction:row;
+  flex:1
 `;
 
-const Quadrado1 = styled.View `
-    flex:1;
-    height:100px;
-    background-color:black;
-    padding:10px;
-    margin-top: 45px;
+const Superior =styled.View`
+   width:500px;
+   height:0px;
+   margin-top: 41px;
+   padding:65px;
+   background-color:black;
 `;
 
-const Quadrado2 = styled.View `
-    flex:1;
-    height:150px;
-    background-color:blue;
-    padding:10px;
-    margin-top: 145px;
+const Mercard = styled.Text`
+    left:100px;
+    top: 40px;
+    color:black;
+    font-size:35px;
 `;
+
+const On = styled.Text`
+    left:220px;
+    top: 10px;
+    color:green;
+    font-size:35px;
+`;
+
+const Line = styled.Text`
+    left:268px;
+    top: -40px;
+    color:black;
+    font-size:35px;
+`;
+
+const Corpo = styled.Text`
+    color:black
+    left:80px;
+    top: -10px;
+    color:black;
+    font-size:15px;
+
+`;  
 
 export default () => {
   return(
     <Page>
-      <Quadrado1></Quadrado1>
-      <Quadrado2></Quadrado2>
+     <Superior></Superior>
+     <Mercard>Mercad</Mercard>
+     <On>On</On>
+     <Line>line</Line>
+     <Corpo>Pedido Carrefour - Em andamento</Corpo>
+     <Icon name="clockcircleo" size={25} color='#000'/>
     </Page>
   );
 }
