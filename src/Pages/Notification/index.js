e6861f62b3afc9d8da9f13ea956d191d26b37fde
RicksonEdward29 <ricksonsilva29@outlook.com>
2022-04-05 17:54:00 -0300

    comecando a segunda tela

--- src/Pages/Notification/index.js
+++ src/Pages/Notification/index.js
@@ -1,22 +1,44 @@
 import React from 'react';
 import { SafeAreaView, Text, View, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign'
+import { color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 export default function Notification() {
     return(
-        <View style={StyleSheet.container}>
-            <Text style={StyleSheet.text}>Pagina Notification</Text>
-        </View>
+        <SafeAreaView style={StyleSheet.container}>
+            <View style={StyleSheet.frontal}>
+             <Text style={frente.texto}>CARREFOUR</Text>
+             <Icon style={frente.icone01} name="arrowleft" size={35} color="green"></Icon>
+            </View>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        justifyContent: 'center',
-        alignItems:'center',
+        backgroundColor:'white',
     },
-    text:{
-        fontSize: 25,
-        fontWeight: 'bold'
-    }
+    
+
 });
+
+const frente = StyleSheet.create({
+    frontal:{
+            flex: 1,
+        width: 500,
+        height: 100,
+        backgroundColor: "black",
+    },
+    texto:{
+        fontSize: 20,
+        color:"black",
+        left: 50,
+        top: 16,
+    },
+    icone01:{
+        marginTop: -15,
+    }
+
+
+})
